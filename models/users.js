ea9f7b7b24322f88726e8c146b723ddf6a0de824
MohmedIkram <53212527+MohmedIkram@users.noreply.github.com>
2021-07-13 14:48:12 +0530

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -1,1 +1,34 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    required: true,
+  },
+  userid: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// model first argument is sigular
+// and in MongoDB the collection will be plural
+// Collection name - "User"
+
+export const User = mongoose.model("User", userSchema);
